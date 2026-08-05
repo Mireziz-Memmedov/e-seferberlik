12825ac3eb5f5c80e2a3162991f919d69b883f29
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-08-05 18:19:38 +0000

    commit

--- faq.js
+++ faq.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
 
+    // FAQ cavabını açıb-bağlama
     $('.faq-question').click(function (e) {
         e.preventDefault();
         $(this).next('.faq-answer').slideToggle();
+    });
+
+    // FAQ-dan kənara kliklədikdə cavabı bağlama
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.faq-question').length && !$(e.target).closest('.faq-answer').length) {
+            $('.faq-answer').slideUp();
+        }
     });
 
 });

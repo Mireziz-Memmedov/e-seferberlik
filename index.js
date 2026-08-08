f78c3b82511773756bbdd075008b654c7e20fc52
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-08-08 10:40:47 +0000

    guest

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
-    $('#faq').click(function (e) { 
+
+    $('#faq').click(function (e) {
         e.preventDefault();
         window.location.href = "./faq.html";
+    });
+
+    $('#guest-login').click(function (e) {
+        e.preventDefault();
+        window.location.href = "./guest.html";
     });
 });
